59f0fb4204b1b0d482f2095c5b21bfc12cfe9c31
cruisemu <cruisemu@gmail.com>
2025-08-16 13:47:59 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -5,10 +5,8 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 # Git稀疏克隆，只克隆指定目录到本地
