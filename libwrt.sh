a30078dddd4cac5c10c5cb7649e9d953551bba01
cruisemu <cruisemu@gmail.com>
2025-08-21 21:22:02 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,2 +1,12 @@
 rm -rf package/emortal/luci-app-athena-led
-git clone --depth=1 https://github.com/sbwml/openwrt_helloworld package/luci-app-ssr-plus
+#git clone --depth=1 https://github.com/sbwml/openwrt_helloworld package/luci-app-ssr-plus
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
