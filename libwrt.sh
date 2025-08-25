99f7d9ddc751aa5640024512f3f81155fc81b355
cruisemu <cruisemu@gmail.com>
2025-08-25 22:52:58 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,5 +1,4 @@
 rm -rf package/emortal/luci-app-athena-led
-git clone --depth=1 https://github.com/sbwml/openwrt_helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/sbwml/openwrt_helloworld package/hysteria
 
 # Git稀疏克隆，只克隆指定目录到本地
